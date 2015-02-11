b2f7f16d183acde27b70f76b7d3f5c3f6d5bd24e
Flipperxxx14 <shawnmichael14@gmail.com>
2015-02-10 21:29:26 -0700

    Create KingMichael-Assn4-main.cpp

---
+++ KingMichael-Assn4-main.cpp
@@ -0,0 +1,24 @@
+// KingMichaelAssn4.cpp : Defines the entry point for the console application.
+//
+
+#include "KingMichael-Assn4-common.h"
+#include "Michael-Assn4-functions.h"
+#include "King-Assn4-functions.h"
+
+int main(int argc, char* argv[])
+{
+	int numTimes = 0;
+	sortType *usrSorts;
+	double *srtAvgs = 0;
+
+	do
+	{
+		showMenu();
+		usrSorts = getUserSort(numTimes);
+		processChoices(usrSorts, numTimes, usrSorts, srtAvgs);
+
+
+	} while (*usrSorts != EXIT);
+
+	return 0;
+}
