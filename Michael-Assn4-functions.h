19901aa189c3057248e9b6950b1acc7483c93b7a
Flipperxxx14 <shawnmichael14@gmail.com>
2015-02-15 14:18:12 -0700

    Create Michael-Assn4-functions.h
    Latest

---
+++ Michael-Assn4-functions.h
@@ -0,0 +1,19 @@
+//*****************************************************************************
+// FILE NAME: Michael-assn4.h
+// DESCRIPTION: This file contains Shawn Michael's function prototypes for
+//              assn 4.
+// CLASS/TERM: CS 372, 15S8W1
+// DESIGNER: Shawn Michael
+//*****************************************************************************
+
+//prototypes
+//#include "KingMichael-Assn4-common.h"
+
+void showMenu();
+sortType * getUserSort(int &numTimes);
+int getTestRuns();
+int * initArray();
+void popArray(int theList[]);
+int * bubbleSortIt(int rdmLst[]);
+void showArray(int rdmLst[]);
+int * quickSort(int rdmLst[], int first, int last);
